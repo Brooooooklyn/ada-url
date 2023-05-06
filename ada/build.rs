2a9e88d9e5d48384d7a1e5c883d7fc508f445b38
LongYinan <lynweklm@gmail.com>
2023-05-06 17:05:40 +0800

    build: fix Windows build

--- ada/build.rs
+++ ada/build.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 fn main() {
     println!("cargo:rerun-if-changed=binding/ada.cpp");
     println!("cargo:rerun-if-changed=binding/ada.hpp");
@@ -9,10 +11,16 @@ fn main() {
         .file("./binding/ada-binding.cpp")
         .include("./binding/ada.hpp")
         .include("./binding/ada-binding.hpp")
-        .cpp(true)
-        .compiler("clang++")
-        .flag("-std=c++17")
-        .cpp_set_stdlib("c++")
-        .compile("ada");
-    println!("cargo:rustc-link-lib=c++");
+        .cpp(true);
+    let compile_target_os = env::var("CARGO_CFG_TARGET_OS").expect("CARGO_CFG_TARGET_OS");
+    let compile_target_env = env::var("CARGO_CFG_TARGET_ENV").expect("CARGO_CFG_TARGET_ENV");
+    if !(compile_target_os == "windows" && compile_target_env == "msvc") {
+        build.compiler("clang++");
+        build.cpp_set_stdlib("c++").flag("-std=c++17");
+        println!("cargo:rustc-link-lib=c++");
+    } else {
+        build.flag("/std:c++17").static_crt(true);
+    }
+
+    build.compile("ada");
 }
